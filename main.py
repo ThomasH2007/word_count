1cf77d99b8b7a346d045a07ce077099601cdeb3f
ThomasH2007 <112638738+ThomasH2007@users.noreply.github.com>
2022-09-22 22:04:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-chaine = input("entrez texte ")
-def word_count():
+#Thomas Hamel 406 
+chaine = input("entrez texte ") #L'utilisateur entre son text 
+def word_count(): 
     print(len(chaine.split(" ")))
-word_count()
+word_count() # la fonction est appélé 
